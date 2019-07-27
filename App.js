ab5a6dab1d724b1af8f7d3f492fa42de1f5654bc
Martin Milev <martinmariusmilev@gmail.com>
2019-07-27 03:53:44 +0300

    rnNYTReader: implement NYT API
    Added API_URL and API_KEY as enviorment variables.
    Added react-native-config library to expose config variables to RN.
    Fetch request results titles are now rendered in the app.

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import React, {Fragment} from 'react'
+import React, { Fragment,  useState, useEffect } from 'react'
 import { Text, StatusBar } from 'react-native'
+import Config from 'react-native-config'
 
-const App = () => (
+const App = () => {
+    const [articles, setArticles] = useState(0)
+
+  useEffect(() => { 
+    let mounted = false
+
+    if (!mounted) {
+      fetch(
+        `${Config.API_URL}/mostpopular/v2/viewed/1.json?api-key=${Config.API_KEY}`
+      )
+      .then(response => response.json())
+      .then(json => setArticles(json.results))
+      .catch((error) => console.error(error))
+      mounted = true
+    }
+  }, [])
+
+
+  return (
   <Fragment>
     <StatusBar backgroundColor="transparent" barStyle="dark-content" />
-    <Text
-      style={{
-        fontSize: 24,
-        alignSelf: 'center',
-        marginTop: 'auto',
-        marginBottom: 'auto'
-      }}
-    >
+    <Text style={{ fontSize: 24, alignSelf: 'center' }}>
       RN New York Times Reader
     </Text>
+    {articles == 0 ? (
+      <Text style={{ fontSize: 24, alignSelf: 'center' }}>
+        Loading...
+      </Text>
+    ) : (
+        {articles.map((article, index) => (
+            <Text key={index}>{index + 1}: {article.title}</Text>
+          )
+        )}
+    )}
   </Fragment>
 )
+}
 
 export default App
