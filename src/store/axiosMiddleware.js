8f791a4a6f6c4512793144fbb3c52a218debbf5b
Martin Milev <martinmariusmilev@gmail.com>
2019-07-28 18:14:23 +0300

    rnNYTReader: added development enviorment
    Added development enviorment with mocked endpoints.

--- src/store/axiosMiddleware.js
+++ src/store/axiosMiddleware.js
@@ -16,4 +16,40 @@ const options = {
   }
 }
 
+if (Config.ENV === 'development') {
+  let MockAdapter = require('axios-mock-adapter')
+  let mock = new MockAdapter(axios)
+
+  mock.onGet(/\/mostpopular\/v2\/viewed\/1/).reply(config => {
+    return [
+      200, {
+        results: [{
+          title: 'Brexit: plans in place to mitigate impact of no deal',
+          abstract: 'If the UK leaves the EU without a deal, the effects will be felt by people and companies across Europe...',
+          published_date: '21-03-2019'
+        }, {
+          title: 'From the new Commission president to migration: 5 numbers from Strasbourg',
+          abstract: 'Parliament elected Ursula von der Leyen as president of the European Commission, with 383 votes out of 733.', 
+          published_date: '11-06-2019'
+        }, {
+          title: 'Brexit: plans in place to mitigate impact of no deal',
+          abstract: 'If the UK leaves the EU without a deal, the effects will be felt by people and companies across Europe...',
+          published_date: '21-03-2019'
+        }, {
+          title: 'From the new Commission president to migration: 5 numbers from Strasbourg',
+          abstract: 'Parliament elected Ursula von der Leyen as president of the European Commission, with 383 votes out of 733.', 
+          published_date: '11-06-2019'
+        }, {
+          title: 'Brexit: plans in place to mitigate impact of no deal',
+          abstract: 'If the UK leaves the EU without a deal, the effects will be felt by people and companies across Europe...',
+          published_date: '21-03-2019'
+        }, {
+          title: 'From the new Commission president to migration: 5 numbers from Strasbourg',
+          abstract: 'Parliament elected Ursula von der Leyen as president of the European Commission, with 383 votes out of 733.', 
+          published_date: '11-06-2019'
+        }]
+      }]
+  })
+}
+
 export default axiosMiddleware(axios, options)
